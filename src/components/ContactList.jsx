e232c4067cf360f5c3f2b7b574b3661285390194
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-06 08:51:37 -0500

    (◕‿◕)

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,40 +1,31 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import ContactRow from "./ContactRow";
 
-// Dummy contacts data (should be outside the component)
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
-
-export default function ContactList() { 
-  // Step 10: Use useState hook to store contact list
+export default function ContactList() {
   const [contacts, setContacts] = useState(dummyContacts);
 
-  // Step 11: Console log contacts to verify state
-  console.log("Contacts: ", contacts);
-
-  return ( 
+  return (
     <table>
       <thead>
         <tr>
           <th colSpan="3">Contact List</th>
         </tr>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-        </tr>
       </thead>
       <tbody>
-        {contacts.map((contact) => (
-          <tr key={contact.id}>
-            <td>{contact.name}</td>
-            <td>{contact.email}</td>
-            <td>{contact.phone}</td>
-          </tr>
-        ))}
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {contacts.map((contact) => {
+          return <ContactRow key={contact.id} contact={contact} />;
+        })}
       </tbody>
     </table>
-  ); 
+  );
 }
