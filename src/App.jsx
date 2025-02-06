4f198b4181adb42023e5d3977d43e4213309fa75
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-06 11:45:13 -0500

    edits up to simple table heading and columns (step 8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,10 @@
-import React from "react";
 import "./App.css";
+import ContactList from "./components/ContactList";
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
