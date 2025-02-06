383c63a4f757e360e26230c98af1d44394705235
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-06 11:27:31 -0500

    reset project in aux branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,2 @@
 import React from "react";
-import ContactList from "./components/ContactList";
 import "./App.css";
-
-export default function App() {
-  return (
-    <>
-      <ContactList />
-    </>
-  );
-}
