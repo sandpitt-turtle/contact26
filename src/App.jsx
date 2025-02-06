d2074fb31b8cdd00995830d40a5a8f317d273148
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-06 12:32:53 -0500

    Up to Step 5 -- breaking & returning, trying to finish as much as possible using past work and not the example answers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { useState } from 'react';
 import ContactList from "./components/ContactList";
 
+
+
+
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
       <ContactList />
