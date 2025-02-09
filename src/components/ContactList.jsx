73689931db211eb90ed8bab232dc2d773a31bb79
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-09 09:55:09 -0500

    On 'Selected Contact View.' Need to fix this

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,15 +1,14 @@
-import {useEffect} from 'react';
-import React, { useState } from "react"; 
+import { useEffect, useState } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+// const dummyContacts = [
+//   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+// ];
 
-export default function ContactList() { 
-  const [contacts, setContacts] = useState(dummyContacts);
+export default function ContactList({ setSelectedContactId }) { 
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     async function fetchContacts() {
@@ -20,12 +19,11 @@ export default function ContactList() {
         );
         const result = await response.json();
         setContacts(result);
-
       } catch (error) {
         console.error(error);
       }
     }
-    fetchContacts()
+    fetchContacts();
   }, []);
 
   return ( 
